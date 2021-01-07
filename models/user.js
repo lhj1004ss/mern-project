6c0b4bf048f8acabe416f1c873e05c752fa1e7d4
hyojelee <hyojelee1004ss@gmail.com>
2021-01-07 14:57:29 -0500

    implemented login with jsonwebtoken and bcrypt

--- models/user.js
+++ models/user.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const userSchema = mongoose.Schema({
   firstname: {
@@ -14,7 +18,6 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    maxlength: 50
   },
   image: String,
   role: {
@@ -22,13 +25,51 @@ const userSchema = mongoose.Schema({
     enum: ['Admin','User'],
     default: 'User'
   },
-  token:{
+  token: {
     type: String
   },
   tokenExp: {
     type: Number
   }
 },{timestamps: true});
+
+userSchema.pre('save', function(next){
+
+  var user = this;
+  // only when password changed, this function works
+  if(user.isModified('password')){
+    bcrypt.genSalt(saltRounds,function(err, salt){
+      if(err) return next(err);
+
+      bcrypt.hash(user.password, salt, function(err, hash){
+      if(err) return next(err);
+      // successfully make hash? then assign hash to plain password
+      user.password = hash;
+      next();
+      })
+    })
+  }else{
+    next();
+  }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+    if(err) return cb(err);
+    cb(null, isMatch);
+  }) 
+}
+
+userSchema.methods.generateToken = function(cb){
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  user.token = token;
+  user.save(function(err,user){
+    if(err) return cb(err);
+    cb(null, user);
+  });
+}
 
 const User = mongoose.model('User',userSchema);
 module.exports = { User };
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
 const app = express()
-const PORT=5000;
+
+const {User} = require('./models/user');
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose.connect(MONGO_URI,{
     useNewUrlParser: true,
@@ -12,6 +25,49 @@ mongoose.connect(MONGO_URI,{
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+// @@ post register
+app.post('/register',(req,res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo)=>{
+    if(err) {
+      console.log(`Something occurred${err}`);
+      return res.json(400);
+  };
+    return res.status(200).json({success: true})
+  });
+});
+
+// @@ post login
+app.post('/login',(req,res) => {
+  User.findOne({ email:req.body.email}, (err,user) => {
+    // if no vaild user
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        message:'there no vaild email you try to login'
+      });
+    }
+    // password check
+    user.comparePassword(req.body.password, (err,isMatch) => {
+      if(err){
+        console.log(err);
+      }
+      if(!isMatch){
+        return res.json({loginSuccess: false, message:'Password is not Match'})
+      }
+
+      // if password is match, make token
+      user.generateToken((err,user) => { 
+        if(err) return res.status(400).send(err);
+
+        //save token into cookie
+        res.cookie("x_auth",user.token).status(200).json({loginSuccess: true, userId: user._id});
+      })
+
+    })
+  })
 })
 
 app.listen(PORT,()=>{console.log(`Serever is successfully connected on ${PORT}`)});
